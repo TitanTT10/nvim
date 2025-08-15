5f16159a526d4aa677739305b56c1789bc19dcfe
TitanTT10 <titan@tenwinkel.org>
2025-08-15 17:05:29 +0200

    removed fileformat icon from lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,13 +1,13 @@
 return {
-    'nvim-lualine/lualine.nvim',
+    "nvim-lualine/lualine.nvim",
     -- dependencies = { 'nvim-tree/nvim-web-devicons' }  -- needed for *fancy* icons etc.
 
     config = function()
         require("lualine").setup({
             options = {
-                theme = "dracula"
-            }
+                theme = "dracula",
+            },
+            sections = { lualine_x = { "encoding", "filetype" } },
         })
-
-    end
+    end,
 }
