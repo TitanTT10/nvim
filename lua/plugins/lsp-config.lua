9a8d13de06981f4b93aae82d5575f16920181e67
TitanTT10 <titan@tenwinkel.org>
2025-08-31 16:53:38 +0200

    removed pylsp from ensure_installed

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -8,7 +8,7 @@ return {
     {
         "mason-org/mason-lspconfig.nvim",
         opts = {
-            ensure_installed = { "lua_ls", "pylsp", "rust_analyzer" },
+            ensure_installed = { "lua_ls", "rust_analyzer" },
         },
         dependencies = {
             { "mason-org/mason.nvim", opts = {} },
