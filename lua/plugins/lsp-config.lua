04820031e456aaeb4eb79fccbee7a667ed791e0a
TitanTT10 <titan@tenwinkel.org>
2025-11-06 22:32:12 +0100

    Changed it so inlay hints are on by default

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -18,41 +18,46 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
+			-- Enable inlay hints globally by default
+			vim.lsp.inlay_hint.enable(true)
 
-		vim.lsp.config("rust_analyzer", {
-			settings = {
-				['rust-analyzer'] = {
-					-- Enable inlay hints
-					inlayHints = {
-						enable = true,
-						-- Customize specific types of hints
-						parameterHints = { enable = true },
-						typeHints = { enable = true },
-						chainingHints = { enable = true },
-						lifetimeElisionHints = { enable = true, useParameterNames = true },
-					},
-					-- Additional Rust analyzer configurations can go here
-					diagnostics = {
-						enable = true,
-						experimental = { enable = true }
+			vim.lsp.config("rust_analyzer", {
+				settings = {
+					['rust-analyzer'] = {
+						-- Enable inlay hints
+						inlayHints = {
+							enable = true,
+							-- Customize specific types of hints
+							parameterHints = { enable = true },
+							typeHints = { enable = true },
+							chainingHints = { enable = true },
+							lifetimeElisionHints = { enable = true, useParameterNames = true },
+						},
+						-- Additional Rust analyzer configurations can go here
+						diagnostics = {
+							enable = true,
+							experimental = { enable = true }
+						}
 					}
-				}
-			},
-			-- Optional: Add keymaps specific to Rust
-			on_attach = function(client, bufnr)
-				-- Inlay hints toggle with notification
-				vim.keymap.set('n', '<leader>uh', function()
-					local current_state = vim.lsp.inlay_hint.is_enabled({bufnr = bufnr})
-					vim.lsp.inlay_hint.enable(not current_state, {bufnr = bufnr})
+				},
+				-- Optional: Add keymaps specific to Rust
+				on_attach = function(client, bufnr)
+					-- Ensure inlay hints are on when LSP attaches
+					vim.lsp.inlay_hints.enable(true, {bufnr = bufnr})
 
-					if current_state then
-						vim.notify("Inlay Hints Disabled", vim.log.levels.WARN)
-					else
-						vim.notify("Inlay Hints Enabled", vim.log.levels.INFO)
-					end
-				end, { buffer = bufnr, desc = 'Toggle Inlay Hints' })
-			end
-		})
+					-- Inlay hints toggle with notification
+					vim.keymap.set('n', '<leader>uh', function()
+						local current_state = vim.lsp.inlay_hint.is_enabled({bufnr = bufnr})
+						vim.lsp.inlay_hint.enable(not current_state, {bufnr = bufnr})
+
+						if current_state then
+							vim.notify("Inlay Hints Disabled", vim.log.levels.WARN)
+						else
+							vim.notify("Inlay Hints Enabled", vim.log.levels.INFO)
+						end
+					end, { buffer = bufnr, desc = 'Toggle Inlay Hints' })
+				end
+			})
 
 			vim.keymap.set("n", "<leader>K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
