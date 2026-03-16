207a3b53313692b154f0860f8ad69ec47ac02e08
TitanTT10 <titan@tenwinkel.org>
2026-03-16 20:26:56 +0100

    Changed color-theme from Catppuccin to TokyoNight

---
+++ lua/plugins/tokyonight.lua
@@ -0,0 +1,12 @@
+return {
+	"folke/tokyonight.nvim",
+	lazy = false,
+	priority = 1000,
+	opts = {},
+	config = function()
+		vim.cmd.colorscheme("tokyonight")
+--		vim.cmd("highlight Normal ctermbg=none guibg=none")
+
+	end,
+}
+--"tokyonight" "tokyonight-night" "tokyonight-storm" "tokyonight-day" "tokyonight-moon"
